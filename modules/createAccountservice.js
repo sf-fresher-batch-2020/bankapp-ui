eeb487fd7d0075fa8155e728cc9f41e390d2e882
PasamPavithra <37284146+PasamPavithra@users.noreply.github.com>
2020-10-21 11:38:42 +0530

    Add files via upload

---
+++ modules/createAccountservice.js
@@ -0,0 +1,11 @@
+class CreateAccountService
+{
+    save(obj){
+            var users =[];
+            var usersTemp = JSON.parse(localStorage.getItem("ACCOUNTS"));
+            var users = usersTemp ? usersTemp : [];
+            users.push(obj);
+            localStorage.setItem("ACCOUNTS", JSON.stringify(users ) );
+            localStorage.getItem("ACCOUNTS");
+    }
+}
